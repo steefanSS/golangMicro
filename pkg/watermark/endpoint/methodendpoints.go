c497d87dc23299921a227e2650b28888e1337a68
stefan.nicic <s.nicic@vegait.rs>
2023-05-25 15:42:06 +0200

    completed endpoints

--- pkg/watermark/endpoint/methodendpoints.go
+++ pkg/watermark/endpoint/methodendpoints.go
@@ -28,3 +28,68 @@ func (s *InterfaceSet) Get(ctx context.Context, filters ...internal.Filter) ([]i
 
 	return getResp.Document, nil
 }
+
+func (s *InterfaceSet) Status(ctx context.Context, ticketID string) (internal.Status, error) {
+	resp, err := s.StatusEndpoint(ctx, StatusRequest{TicketId: ticketID})
+
+	if err != nil {
+		return internal.Failed, err
+	}
+
+	stsResp := resp.(StatusResponse)
+
+	if stsResp.Err != "" {
+		return internal.Failed, errors.New(stsResp.Err)
+	}
+
+	return stsResp.Status, nil
+
+}
+
+func (s *InterfaceSet) AddDocument(ctx context.Context, doc *internal.Document) (string, error) {
+	resp, err := s.AddDocumentEndPoint(ctx, AddDocumentRequest{Document: doc})
+
+	if err != nil {
+		return "", err
+	}
+
+	addDocResponse := resp.(AddDocumentResponse)
+
+	if addDocResponse.Err != "" {
+		return "", errors.New(addDocResponse.Err)
+	}
+
+	return addDocResponse.TicketId, nil
+}
+
+func (s *InterfaceSet) Watermark(ctx context.Context, ticketID, mark string) (int, error) {
+	resp, err := s.WatermarkEndpoint(ctx, WatermarkRequest{TickedId: ticketID, Mark: mark})
+
+	wmResp := resp.(WatermarkResponse)
+
+	if err != nil {
+		return wmResp.Code, err
+	}
+
+	if wmResp.Err != "" {
+		return wmResp.Code, errors.New(wmResp.Err)
+	}
+
+	return wmResp.Code, nil
+}
+
+func (s *InterfaceSet) ServiceStatus(ctx context.Context) (int, error) {
+	resp, err := s.ServiceStatusEndpoint(ctx, ServiceStatusRequest{})
+
+	serviceStatus := resp.(ServiceStatusResponse)
+
+	if err != nil {
+		return serviceStatus.Code, err
+	}
+
+	if serviceStatus.Err != "" {
+		return serviceStatus.Code, errors.New(serviceStatus.Err)
+	}
+
+	return serviceStatus.Code, nil
+}
